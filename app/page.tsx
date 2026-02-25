9a66c01a5ea87c6be2f268183933120a2db11c54
OleksandrLukomskyi <LUKOMSKYI.OLEKSANDR@GMAIL.COM>
2026-02-25 23:30:53 +0100

    feat: add  add-company-button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import StatusLabel, { Status } from '@/app/components/status-label';
+import { headers } from 'next/headers';
 
-export default function Home() {
+export default async function Home() {
+  const h = await headers();
+  console.log(Object.fromEntries(h.entries()));
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
+      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
