4c086c4ee56e5a83f1a42bc9fd65034594fe35a8
OleksandrLukomskyi <LUKOMSKYI.OLEKSANDR@GMAIL.COM>
2026-03-28 12:37:10 +0100

    update homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import { headers } from 'next/headers';
 import AddCompanyButton from '../components/add-company-button';
 
 export default async function Home() {
-  const h = await headers();
-  console.log(Object.fromEntries(h.entries()));
   return (
     <main>
       <h1 className="text-xl">Home page </h1>
