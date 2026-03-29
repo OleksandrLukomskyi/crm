b6a3e484eda767a71b0c4077568045989cdaa408
OleksandrLukomskyi <LUKOMSKYI.OLEKSANDR@GMAIL.COM>
2026-03-29 14:54:39 +0200

    fix

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,12 +1,12 @@
-import MagicButton from '@/src/components/magic-button';
 import React from 'react';
+import Header from '@/src/components/header';
+
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
   return (
-    <main>
-      <h1 className="text-xl">Dashboard Page</h1>
-      <MagicButton />
-    </main>
+    <>
+      <Header>Dashboard</Header>
+    </>
   );
 }
