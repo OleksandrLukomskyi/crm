c831b5bb01e94b6037240fa77e16330086072407
OleksandrLukomskyi <LUKOMSKYI.OLEKSANDR@GMAIL.COM>
2026-04-04 23:08:19 +0200

    chore: remove unused components from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import AddCompanyButton from '../components/add-company-button';
-import MagicButton from '../components/magic-button';
-
 export default async function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page </h1>
-
-      <AddCompanyButton />
-      <MagicButton />
     </main>
   );
 }
